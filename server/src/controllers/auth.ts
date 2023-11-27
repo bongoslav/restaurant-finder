701fc1f06cd626a8158ef0d14d8262ba2f5e348a
bongoslav <beborisov99@gmail.com>
2023-11-27 16:49:42 +0200

    added /me route

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt"
 import User from "../models/user.model";
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv";
+import { AuthenticatedRequest } from "../types";
 dotenv.config();
 
 export const getUsers = async (req: Request, res: Response) => {
@@ -66,5 +67,14 @@ export const register = async (req: Request, res: Response) => {
   } catch (err) {
     console.error("Registration error:", err); // Log the specific error for debugging purposes
     return res.status(500).json({ message: "Something went wrong while registering." });
+  }
+}
+
+export const getCurrentUser = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const user = await User.findOne({ where: { email: req.userData.email } })
+    return res.status(200).json({ user })
+  } catch (err) {
+    return res.status(404).json({ message: "User not found" })
   }
 }
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,11 @@
 import express from "express"
-import { getUsers, login, register } from "../controllers/auth";
+import { getCurrentUser, getUsers, login, register } from "../controllers/auth";
 import { isLoggedIn } from "../middlewares/loggedIn";
 
 const router = express();
 
 router.get("/api/v1/users", isLoggedIn, getUsers);
+router.get("/api/v1/me", isLoggedIn, getCurrentUser);
 router.post("/api/v1/login", login);
 router.post("/api/v1/register", register);
 
