14db7324ec2d8151d0df9564cfbb456a9332d9af
bongoslav <beborisov99@gmail.com>
2023-11-27 16:49:25 +0200

    added isOwner middleware

---
+++ server/src/middlewares/owner.ts
@@ -0,0 +1,28 @@
+import { Response, NextFunction } from "express";
+import User from "../models/user.model";
+import { AuthenticatedRequest } from "../types";
+import Restaurant from "../models/restaurant.model";
+
+export const isOwner = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  let restaurant: Restaurant;
+  let user: User;
+  try {
+    user = await User.findOne({ where: { email: req.userData.email } })
+  } catch (err) {
+    return res.status(400).json({ message: "User not found" })
+  }
+
+  try {
+    restaurant = await Restaurant.findOne({ where: { id: req.params.id } })
+  } catch (err) {
+    return res.status(400).json({ message: "Restaurant not found" })
+  }
+
+  const ownerId = restaurant.ownerId
+
+  if (ownerId != user.id) {
+    return res.status(400).json({ message: "Logged in user is not an owner" })
+  }
+
+  next();
+}
--- server/src/routes/restaurants.ts
+++ server/src/routes/restaurants.ts
@@ -10,14 +10,15 @@ import {
 } from "../controllers/restaurants";
 import uploadRestaurantPhoto from "../middlewares/uploadRestaurantPhoto";
 import { isLoggedIn } from "../middlewares/loggedIn";
+import { isOwner } from "../middlewares/owner";
 
 const router = express();
 
 router.get("/api/v1/restaurants", getAllRestaurantsWithReviews);
 router.get("/api/v1/restaurants/:id", getRestaurantWithReviews);
-router.post("/api/v1/restaurants/add-restaurant", isLoggedIn, createRestaurant);
-router.patch("/api/v1/restaurants/:id", isLoggedIn, updateRestaurant);
-router.delete("/api/v1/restaurants/:id", isLoggedIn, deleteRestaurant);
+router.post("/api/v1/restaurants/add-restaurant", isLoggedIn, isOwner, createRestaurant);
+router.patch("/api/v1/restaurants/:id", isLoggedIn, isOwner, updateRestaurant);
+router.delete("/api/v1/restaurants/:id", isLoggedIn, isOwner, deleteRestaurant);
 
 router.post("/api/v1/restaurants/:id/add-photo",
   isLoggedIn,
