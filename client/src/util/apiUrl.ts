ae552f200f8f4bd7efad4824b406e4077a8d6225
Borislav Borisov <beborisov99@gmail.com>
2024-08-12 13:26:41 +0300

    [fix] minor bugfix logic prod nev

--- client/src/util/apiUrl.ts
+++ client/src/util/apiUrl.ts
@@ -1,6 +1,6 @@
 const VITE_ENV = import.meta.env.VITE_ENV;
-const VITE_DEV_API_URL = import.meta.env.VITE_PROD_API_URL;
-const VITE_PROD_API_URL = import.meta.env.VITE_DEV_API_URL;
+const VITE_DEV_API_URL = import.meta.env.VITE_DEV_API_URL;
+const VITE_PROD_API_URL = import.meta.env.VITE_PROD_API_URL;
 
 const API_URL = VITE_ENV === "PROD" ? VITE_PROD_API_URL : VITE_DEV_API_URL;
 
