9834f17f1060a2cd28d4d49b5d701a6b64a01909
Borislav Borisov <beborisov99@gmail.com>
2024-08-12 14:16:33 +0300

    [config] updated prod cookies/cors config

--- server/src/utils/setCookie.ts
+++ server/src/utils/setCookie.ts
@@ -10,7 +10,7 @@ export const setCookie = (
   value: string,
   options: ExtendedCookieOptions = {}
 ): void => {
-  const isProduction = process.env.NODE_ENV === "production";
+  const isProduction = process.env.NODE_ENV === "PROD";
 
   const defaultOptions: ExtendedCookieOptions = {
     httpOnly: true,
